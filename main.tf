4e48328d5557d1a8322a26fbc350d13f4098f950
xsully12 <sullivan.camargo@fatec.sp.gov.br>
2023-02-25 12:35:48 -0300

    Subindo os arquivos

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+    region  = "us-west-2"
+    profile    = "default"
+  
+}
+
+resource "aws_instance" "app_server" {
+    ami           = "ami-0735c191cf914754d"
+    instance_type = "t2.micro"
+    key_name = "iac-sully"
+    #user_data = <<-EOF
+    #           #!/bin/bash
+    #           cd /home/ubuntu
+    #          echo "<h1>Feito com terraform</h1>" > index.html
+    #           nohup busybox httpd -f -p 8080 &
+    #EOF
+    tags = {
+    Name = "Teste aws"
+  }
+}
